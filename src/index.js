ce3be7aea166f6a715cd13457100d050d7e727a6
jaime <jbenaci736@g.educaaand.es>
2023-02-12 14:22:38 +0100

    Añadido permiso cors a frontend

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const express = require("express")
 const app = express()
-// const cors = require("cors")
+const cors = require("cors")
 const rutasV1 = require("./routes/v1/indexRoutes")
 
-// app.use(cors())
+app.use(cors({
+  origin: 'idle-dev.netlify.app'
+}))
 app.use(express.json())
 app.use("/api/v1", rutasV1.router)
 
